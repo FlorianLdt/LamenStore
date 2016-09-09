1e03d6b6388c9c4bc5803c9b2be3661754257e0b
Florian <florian.ludot@gmail.com>
2016-09-10 00:32:18 +0200

    LamenStore Starting Workspace

--- LamenStore/AppDelegate.swift
+++ LamenStore/AppDelegate.swift
@@ -15,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.makeKeyAndVisible()
+        
+        let layout = UICollectionViewFlowLayout()
+        let featuredAppsController = FeaturedAppsController(collectionViewLayout: layout)
+        window?.rootViewController = UINavigationController(rootViewController: featuredAppsController)
+        
         // Override point for customization after application launch.
         return true
     }
