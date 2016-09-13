393fa53b44e1436abcb57dcffe23e44e1538b667
Florian <florian.ludot@gmail.com>
2016-09-13 22:29:27 +0200

    Completed Part-1

--- LamenStore/ViewController.swift
+++ LamenStore/ViewController.swift
@@ -8,17 +8,27 @@
 
 import UIKit
 
-class FeaturedAppsController: UICollectionViewController {
+class FeaturedAppsController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
+    private let cellId = "cellId"
     override func viewDidLoad() {
         super.viewDidLoad()
         
     collectionView?.backgroundColor = UIColor.whiteColor()
+        
+    collectionView?.registerClass(CategoryCell.self, forCellWithReuseIdentifier: cellId)
     }
-
-   
     
-
-
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellId, forIndexPath: indexPath) as! CategoryCell
+        return cell
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSizeMake(view.frame.width, 230)
+    }
 }
-
